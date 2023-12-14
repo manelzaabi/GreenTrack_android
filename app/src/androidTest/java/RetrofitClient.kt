5aab3fbcd31416a17ccc4a7fad0330dd9ba541ee
bechir105 <mohamedbechir.kefi@esprit.tn>
2023-12-14 09:44:33 +0100

    fixing

---
+++ app/src/androidTest/java/RetrofitClient.kt
@@ -0,0 +1,20 @@
+import android.util.Log
+import com.example.videos.api.ApiService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://172.16.5.147:8000/"
+
+    val apiService: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        Log.d("ApiEndpoint", "API Endpoint: ${retrofit.baseUrl()}video/add")
+
+        retrofit.create(ApiService::class.java)
+    }
+}
+
