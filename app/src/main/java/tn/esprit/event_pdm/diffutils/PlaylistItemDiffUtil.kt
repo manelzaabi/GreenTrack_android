14742751310713b51695121baf7afc1ac62ef080
bechir105 <mohamedbechir.kefi@esprit.tn>
2023-12-14 09:43:11 +0100

    youtubetest

---
+++ app/src/main/java/tn/esprit/event_pdm/diffutils/PlaylistItemDiffUtil.kt
@@ -0,0 +1,30 @@
+package com.example.videos.diffutils
+
+
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.videos.model.PlaylistYtModel
+import com.example.videos.model.VideoYtModel
+
+class PlaylistItemDiffUtil(
+    private val oldList: List<PlaylistYtModel.PlaylistItem>,
+    private val newList: List<PlaylistYtModel.PlaylistItem>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldVideo = oldList[oldItemPosition]
+        val newVideo = newList[newItemPosition]
+        return oldVideo.snippetYt.title == newVideo.snippetYt.title
+    }
+}
